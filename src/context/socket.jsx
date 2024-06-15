91234a4dd781cb0318adc1106e7aedc4c3f26d49
shivam kumar <meforcoding@gmail.com>
2024-06-15 10:19:22 +0530

    ignored env file and added allow origin header

--- src/context/socket.jsx
+++ src/context/socket.jsx
@@ -13,6 +13,11 @@ const SocketProvider = ({ children }) => {
 		if (!socketRef.current) {
 			const socketInstance = io(import.meta.env.VITE_BASE_URL, {
 				withCredentials: true,
+				extraHeaders: {
+					"Access-Control-Allow-Origin": import.meta.env
+						.VITE_BASE_URL,
+					// "https://we-chat-2000.netlify.app",
+				},
 			});
 
 			socketInstance.on("connect", () => {
